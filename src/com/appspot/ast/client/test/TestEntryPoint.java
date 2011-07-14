c12c658d5ac6d7d0458be52922dd968a8434a060
Anton Starcev <ast@design.ru>
2011-07-14 13:11:00 +0400

    First commit

---
+++ src/com/appspot/ast/client/test/TestEntryPoint.java
@@ -0,0 +1,18 @@
+package com.appspot.ast.client.test;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: anton
+ * Date: 14.07.11
+ * Time: 12:49
+ */
+public class TestEntryPoint implements EntryPoint {
+  public void onModuleLoad() {
+    RootPanel.get().add(new HTML("Wave Editor"));
+  }
+}
