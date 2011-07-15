9a764927bf9afff35b565874bef2c259445ea9b5
Anton Starcev <ast@design.ru>
2011-07-15 12:42:06 +0400

    Style panel added

--- src/com/appspot/ast/client/layout/WaveEditor.java
+++ src/com/appspot/ast/client/layout/WaveEditor.java
@@ -1,6 +1,7 @@
 package com.appspot.ast.client.layout;
 
 import com.appspot.ast.client.editor.Toolbar;
+import com.appspot.ast.client.editor.doodad.phone.PhoneWidget;
 import com.appspot.ast.client.editor.harness.GenericHarness;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.DivElement;
@@ -10,6 +11,8 @@ import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
@@ -49,8 +52,9 @@ public class WaveEditor extends Composite {
   @UiField RadioButton wysiwygTab;
   @UiField RadioButton sourceTab;
   @UiField CheckBox logTab;
-  @UiField
-  DivElement richEditorContainer;
+  @UiField DivElement richEditorContainer;
+
+  @UiField PhoneWidget phoneDoodad;
 
   public WaveEditor() {
     harness = new GenericHarness();
@@ -60,6 +64,17 @@ public class WaveEditor extends Composite {
     harness.addUpdateListener(sourceAdopted);
     harness.enableLog(log);
     handleClickLogTab(null);
+
+    initStylePanel();
+  }
+
+  private void initStylePanel() {
+    final Element code = DOM.createElement("span");
+    code.setInnerText("code");
+    final Element number = DOM.createElement("span");
+    number.setInnerText("number");
+    phoneDoodad.getContainer().appendChild(code);
+    phoneDoodad.getContainer().appendChild(number);
   }
 
   public void setText(String text) {
