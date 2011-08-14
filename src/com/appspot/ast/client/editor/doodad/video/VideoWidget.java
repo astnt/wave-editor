b6e813e3450256b2a411e4f042f9f4fe9756a702
Anton Starcev <ast@design.ru>
2011-08-14 13:25:17 +0400

    Video widget upgraded

--- src/com/appspot/ast/client/editor/doodad/video/VideoWidget.java
+++ src/com/appspot/ast/client/editor/doodad/video/VideoWidget.java
@@ -3,8 +3,12 @@ package com.appspot.ast.client.editor.doodad.video;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.IFrameElement;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
 
@@ -15,20 +19,26 @@ import com.google.gwt.user.client.ui.HTMLPanel;
  * Time: 7:37 PM
  */
 public class VideoWidget extends Composite {
+  interface Binder extends UiBinder<HTMLPanel, VideoWidget> { Binder BINDER = GWT.create(Binder.class); }
+
   @UiField IFrameElement iframe;
+  @UiField Button changeUrl;
 
   public void setVideoSrc(String src) {
     iframe.setAttribute("src", src);
   }
 
-  interface Binder extends UiBinder<HTMLPanel, VideoWidget> { Binder BINDER = GWT.create(Binder.class); }
-
   public VideoWidget() {
     initWidget(Binder.BINDER.createAndBindUi(this));
-//    iframe.setAttribute("href", "http://www.youtube.com/embed/5ZxRuxSZfzw");
   }
 
   public Element getContainer() {
     return iframe;
+  }
+
+  @UiHandler("changeUrl")
+  public void handleClickChangeUrl(ClickEvent event) {
+    String updatedUrl = Window.prompt("URL:", "http://www.youtube.com/embed/Ot2NYFFO-2Q");
+    setVideoSrc(updatedUrl);
   }
 }
